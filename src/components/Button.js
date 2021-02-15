3ca754c8de8be01cec12a9312262c2fe32b2743e
JSmith604 <jacquelinesmith190@gmail.com>
2021-02-15 14:20:15 -0800

    Build and test buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,28 @@ import React from "react";
 
 import "components/Button.scss";
 
+import Classnames from "classnames";
+
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   class Button extends React.Component {
+      render () {
+         let buttonClass = classNames({
+            button: true,
+            "button--confirm": props.confirm,
+            "button--danger": props.danger,
+         });
+      }
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
