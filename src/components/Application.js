bbf935986fbf14900e5a92188686c3c136a7a3a7
JSmith604 <jacquelinesmith190@gmail.com>
2021-02-17 21:23:44 -0800

    Render appointments on website

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,64 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
+import Appointment from "./Appointment"
 
-import "components/Application.scss";
+import "./Application.scss";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Alice Jones",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar:"https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Mark McMorris",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar:"https://i.imgur.com/T2WwVfS.png"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Chloe Kim",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  }
+];
+
 
 const days = [
   {
@@ -23,6 +80,11 @@ const days = [
 
 export default function Application(props) {
  const [selectedDay, selectDay] = useState("Monday");
+ const interviewSchedule = appointments.map (appointment => {
+   return (
+     <Appointment key={appointment.id} {...appointment} />
+     )
+ })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -38,7 +100,6 @@ export default function Application(props) {
             day={selectedDay}
             setDay={selectDay}
             />
-
           </nav>
           <img
          className="sidebar__lhl sidebar--centered"
@@ -47,10 +108,12 @@ export default function Application(props) {
           />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {interviewSchedule} 
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
 
 
